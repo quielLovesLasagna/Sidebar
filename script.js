9270c77b6203a7dee011cf030152f1d4023e72f6
Exequiel Arco <137391972+quielloveslasana@users.noreply.github.com>
2023-10-10 16:07:04 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", () =>
+  sidebar.classList.toggle("show-sidebar")
+);
+
+closeBtn.addEventListener("click", () =>
+  sidebar.classList.remove("show-sidebar")
+);
