3845ed82ff44fb56fd33c38f95c07ae538237bfe
Exequiel Arco <exequielarco23@gmail.com>
2023-10-13 21:13:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 "use strict";
 
+// ---- ELEMENTS ---- //
 const toggleBtn = document.querySelector(".sidebar-toggle");
 const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
+// ---- ELEMENTS ---- //
 
-toggleBtn.addEventListener("click", () =>
-  sidebar.classList.toggle("show-sidebar")
-);
+// ---- FUNCTIONS ---- //
+// Open sidebar
+const openSidebar = () => sidebar.classList.toggle("show-sidebar");
 
-closeBtn.addEventListener("click", () =>
-  sidebar.classList.remove("show-sidebar")
-);
+// Close sidebar
+const closeSidebar = () => sidebar.classList.remove("show-sidebar");
+// ---- END OF FUNCTIONS ---- //
+
+// ---- EVENT HANDLERS ---- //
+// Toggle button handler
+toggleBtn.addEventListener("click", openSidebar);
+
+// Close button handler
+closeBtn.addEventListener("click", closeSidebar);
+// ---- END OF EVENT HANDLERS ---- //
